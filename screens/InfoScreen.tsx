36308c72c0a89169dbdafdd7123f605f4bba8426
SabrinaM2 <73124761+SabrinaM2@users.noreply.github.com>
2021-11-04 16:28:18 +0100

    Add files via upload

---
+++ screens/InfoScreen.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import { View, Text } from 'react-native';
+
+const InfoScreen = () => {
+
+    return (
+        <View style={styles.container}>
+            <Text>
+                Bienvenue dans l'espace informations !
+            </Text>
+        </View>
+    )
+}
+
+export default InfoScreen;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
