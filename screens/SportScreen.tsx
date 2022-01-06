2aea18945820ff72d100c9c3d72b5190e6912cad
SabrinaM2 <73124761+SabrinaM2@users.noreply.github.com>
2022-01-06 14:03:26 +0100

    Add files via upload

--- screens/SportScreen.tsx
+++ screens/SportScreen.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
 
-import { View, Text } from 'react-native';
+import { View, Text } from "react-native";
 
 const SportScreen = () => {
-
     return (
         <View style={styles.container}>
             <Text>
@@ -13,13 +12,12 @@ const SportScreen = () => {
         </View>
     )
 }
-
 export default SportScreen;
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-  });
+});
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
@@ -8,7 +7,7 @@ import { RootTabScreenProps } from '../types';
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Sport</Text>
+      <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
