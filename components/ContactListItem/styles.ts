6281c1150e88dafba693b545e75fbed1562abb0f
SabrinaM2 <73124761+SabrinaM2@users.noreply.github.com>
2022-01-06 14:00:59 +0100

    Add files via upload

---
+++ components/ContactListItem/styles.ts
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        width: "100%",
+        justifyContent: 'space-between',
+        padding: 10,
+    },
+    leftContainer: {
+        flexDirection: 'row',
+    },
+    midContainer: {
+        justifyContent: 'space-around',
+    },
+    avatar: {
+        width: 60,
+        height: 60,
+        borderRadius: 50,
+        marginRight: 15,
+    },
+    username: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    status: {
+        
+    }
+});
+export default style;
